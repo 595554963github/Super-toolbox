b42bf4f45da4d5b8daf7e45e84f3047d8dd55e2d
Neptune <595554963@qq.com>
2025-12-08 03:23:09 +0800

    Update SuperToolbox.Designer.cs

--- SuperToolbox.Designer.cs
+++ SuperToolbox.Designer.cs
@@ -2,15 +2,8 @@ namespace super_toolbox
 {
     partial class SuperToolbox
     {
-        /// <summary>
-        /// 必需的设计器变量。
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// 清理所有正在使用的资源。
-        /// </summary>
-        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,10 +15,6 @@ namespace super_toolbox
 
         #region Windows 窗体设计器生成的代码
 
-        /// <summary>
-        /// 设计器支持所需的方法 - 不要修改
-        /// 使用代码编辑器修改此方法的内容。
-        /// </summary>
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
@@ -42,9 +31,7 @@ namespace super_toolbox
             toolTip1 = new ToolTip(components);
             treeViewContextMenu.SuspendLayout();
             SuspendLayout();
-            // 
-            // treeView1
-            // 
+            
             treeView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             treeView1.ContextMenuStrip = treeViewContextMenu;
             treeView1.HideSelection = false;
@@ -54,44 +41,32 @@ namespace super_toolbox
             treeView1.TabIndex = 0;
             toolTip1.SetToolTip(treeView1, "每个工具都有不同的作用，具体使用方法请查看使用手册");
             treeView1.AfterSelect += treeView1_AfterSelect;
-            // 
-            // treeViewContextMenu
-            // 
+            
             treeViewContextMenu.ImageScalingSize = new Size(20, 20);
             treeViewContextMenu.Items.AddRange(new ToolStripItem[] { addNewCategoryMenuItem, renameCategoryMenuItem, deleteCategoryMenuItem, moveToCategoryMenuItem });
             treeViewContextMenu.Name = "treeViewContextMenu";
             treeViewContextMenu.Size = new Size(137, 92);
             treeViewContextMenu.Opening += treeViewContextMenu_Opening;
-            // 
-            // addNewCategoryMenuItem
-            // 
+            
             addNewCategoryMenuItem.Name = "addNewCategoryMenuItem";
             addNewCategoryMenuItem.Size = new Size(136, 22);
             addNewCategoryMenuItem.Text = "添加新分组";
             addNewCategoryMenuItem.Click += addNewCategoryMenuItem_Click;
-            // 
-            // renameCategoryMenuItem
-            // 
+            
             renameCategoryMenuItem.Name = "renameCategoryMenuItem";
             renameCategoryMenuItem.Size = new Size(136, 22);
             renameCategoryMenuItem.Text = "编辑分组";
             renameCategoryMenuItem.Click += renameCategoryMenuItem_Click;
-            // 
-            // deleteCategoryMenuItem
-            // 
+            
             deleteCategoryMenuItem.Name = "deleteCategoryMenuItem";
             deleteCategoryMenuItem.Size = new Size(136, 22);
             deleteCategoryMenuItem.Text = "删除分组";
             deleteCategoryMenuItem.Click += deleteCategoryMenuItem_Click;
-            // 
-            // moveToCategoryMenuItem
-            // 
+            
             moveToCategoryMenuItem.Name = "moveToCategoryMenuItem";
             moveToCategoryMenuItem.Size = new Size(136, 22);
             moveToCategoryMenuItem.Text = "移动到分组";
-            // 
-            // txtFolderPath
-            // 
+            
             txtFolderPath.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             txtFolderPath.ForeColor = SystemColors.ActiveCaptionText;
             txtFolderPath.Location = new Point(283, 12);
@@ -99,9 +74,7 @@ namespace super_toolbox
             txtFolderPath.Size = new Size(499, 23);
             txtFolderPath.TabIndex = 1;
             toolTip1.SetToolTip(txtFolderPath, "拖放一个文件夹到此");
-            // 
-            // btnExtract
-            // 
+            
             btnExtract.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             btnExtract.Font = new Font("微软雅黑", 20F, FontStyle.Bold);
             btnExtract.ForeColor = Color.SpringGreen;
@@ -113,9 +86,7 @@ namespace super_toolbox
             toolTip1.SetToolTip(btnExtract, "点击此按钮开始提取、转换、压缩、解压、打包");
             btnExtract.UseVisualStyleBackColor = true;
             btnExtract.Click += btnExtract_Click;
-            // 
-            // richTextBox1
-            // 
+            
             richTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             richTextBox1.ForeColor = SystemColors.ActiveCaptionText;
             richTextBox1.Location = new Point(283, 80);
@@ -124,9 +95,7 @@ namespace super_toolbox
             richTextBox1.TabIndex = 4;
             richTextBox1.Text = "";
             toolTip1.SetToolTip(richTextBox1, "你可以在此窗口查看提取出来的所有文件信息");
-            // 
-            // btnClear
-            // 
+            
             btnClear.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             btnClear.Font = new Font("Microsoft YaHei UI", 12F);
             btnClear.Location = new Point(694, 46);
@@ -137,16 +106,12 @@ namespace super_toolbox
             toolTip1.SetToolTip(btnClear, "点击此按钮清空所有提取出来的文件信息");
             btnClear.UseVisualStyleBackColor = true;
             btnClear.Click += btnClear_Click;
-            // 
-            // toolTip1
-            // 
+            
             toolTip1.AutoPopDelay = 5000;
             toolTip1.InitialDelay = 500;
             toolTip1.ReshowDelay = 100;
             toolTip1.ShowAlways = true;
-            // 
-            // SuperToolbox
-            // 
+            
             AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(888, 571);
