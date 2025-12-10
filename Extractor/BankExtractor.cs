22e49c38cae81aded518a55fd8258874ddb58d9e
Neptune <595554963@qq.com>
2025-12-11 05:13:27 +0800

    Update BankExtractor.cs

--- Extractor/BankExtractor.cs
+++ Extractor/BankExtractor.cs
@@ -41,7 +41,7 @@ namespace super_toolbox
             return bankDataList;
         }
 
-        private static int IndexOf(byte[] source, byte[] pattern, int startIndex)
+        private new static int IndexOf(byte[] source, byte[] pattern, int startIndex)
         {
             for (int i = startIndex; i <= source.Length - pattern.Length; i++)
             {
