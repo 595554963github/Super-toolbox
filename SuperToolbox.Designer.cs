007cd476de6da31e22238b3e4a2378f983c6ca42
Neptune <595554963@qq.com>
2026-02-26 03:20:53 +0800

    Update SuperToolbox.Designer.cs

--- SuperToolbox.Designer.cs
+++ SuperToolbox.Designer.cs
@@ -31,7 +31,9 @@ namespace super_toolbox
             toolTip1 = new ToolTip(components);
             treeViewContextMenu.SuspendLayout();
             SuspendLayout();
-            
+            // 
+            // treeView1
+            // 
             treeView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             treeView1.ContextMenuStrip = treeViewContextMenu;
             treeView1.HideSelection = false;
@@ -39,34 +41,46 @@ namespace super_toolbox
             treeView1.Name = "treeView1";
             treeView1.Size = new Size(265, 535);
             treeView1.TabIndex = 0;
-            toolTip1.SetToolTip(treeView1, "每个工具都有不同的作用，具体使用方法请查看使用手册");
+            toolTip1.SetToolTip(treeView1, "每个工具都有不同的作用,具体使用方法请查看使用手册");
             treeView1.AfterSelect += treeView1_AfterSelect;
-            
+            // 
+            // treeViewContextMenu
+            // 
             treeViewContextMenu.ImageScalingSize = new Size(20, 20);
             treeViewContextMenu.Items.AddRange(new ToolStripItem[] { addNewCategoryMenuItem, renameCategoryMenuItem, deleteCategoryMenuItem, moveToCategoryMenuItem });
             treeViewContextMenu.Name = "treeViewContextMenu";
             treeViewContextMenu.Size = new Size(137, 92);
             treeViewContextMenu.Opening += treeViewContextMenu_Opening;
-            
+            // 
+            // addNewCategoryMenuItem
+            // 
             addNewCategoryMenuItem.Name = "addNewCategoryMenuItem";
             addNewCategoryMenuItem.Size = new Size(136, 22);
             addNewCategoryMenuItem.Text = "添加新分组";
             addNewCategoryMenuItem.Click += addNewCategoryMenuItem_Click;
-            
+            // 
+            // renameCategoryMenuItem
+            // 
             renameCategoryMenuItem.Name = "renameCategoryMenuItem";
             renameCategoryMenuItem.Size = new Size(136, 22);
             renameCategoryMenuItem.Text = "编辑分组";
             renameCategoryMenuItem.Click += renameCategoryMenuItem_Click;
-            
+            // 
+            // deleteCategoryMenuItem
+            // 
             deleteCategoryMenuItem.Name = "deleteCategoryMenuItem";
             deleteCategoryMenuItem.Size = new Size(136, 22);
             deleteCategoryMenuItem.Text = "删除分组";
             deleteCategoryMenuItem.Click += deleteCategoryMenuItem_Click;
-            
+            // 
+            // moveToCategoryMenuItem
+            // 
             moveToCategoryMenuItem.Name = "moveToCategoryMenuItem";
             moveToCategoryMenuItem.Size = new Size(136, 22);
             moveToCategoryMenuItem.Text = "移动到分组";
-            
+            // 
+            // txtFolderPath
+            // 
             txtFolderPath.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             txtFolderPath.ForeColor = SystemColors.ActiveCaptionText;
             txtFolderPath.Location = new Point(283, 12);
@@ -74,7 +88,9 @@ namespace super_toolbox
             txtFolderPath.Size = new Size(499, 23);
             txtFolderPath.TabIndex = 1;
             toolTip1.SetToolTip(txtFolderPath, "拖放一个文件夹到此");
-            
+            // 
+            // btnExtract
+            // 
             btnExtract.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             btnExtract.Font = new Font("微软雅黑", 20F, FontStyle.Bold);
             btnExtract.ForeColor = Color.SpringGreen;
@@ -86,16 +102,22 @@ namespace super_toolbox
             toolTip1.SetToolTip(btnExtract, "点击此按钮开始提取、转换、压缩、解压、打包");
             btnExtract.UseVisualStyleBackColor = true;
             btnExtract.Click += btnExtract_Click;
-            
+            // 
+            // richTextBox1
+            // 
             richTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             richTextBox1.ForeColor = SystemColors.ActiveCaptionText;
+            richTextBox1.HideSelection = false;
             richTextBox1.Location = new Point(283, 80);
             richTextBox1.Name = "richTextBox1";
+            richTextBox1.ScrollBars = RichTextBoxScrollBars.Vertical;
             richTextBox1.Size = new Size(593, 467);
             richTextBox1.TabIndex = 4;
             richTextBox1.Text = "";
             toolTip1.SetToolTip(richTextBox1, "你可以在此窗口查看提取出来的所有文件信息");
-            
+            // 
+            // btnClear
+            // 
             btnClear.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             btnClear.Font = new Font("Microsoft YaHei UI", 12F);
             btnClear.Location = new Point(694, 46);
@@ -106,15 +128,32 @@ namespace super_toolbox
             toolTip1.SetToolTip(btnClear, "点击此按钮清空所有提取出来的文件信息");
             btnClear.UseVisualStyleBackColor = true;
             btnClear.Click += btnClear_Click;
-            
+            // 
+            // toolTip1
+            // 
             toolTip1.AutoPopDelay = 5000;
             toolTip1.InitialDelay = 500;
             toolTip1.ReshowDelay = 100;
             toolTip1.ShowAlways = true;
-            
+            // 
+            // btnAbout
+            //
+            btnAbout = new Button();
+            btnAbout.ForeColor = Color.Blue;
+            btnAbout.Location = new Point(579, 46);
+            btnAbout.Name = "btnAbout";
+            btnAbout.Size = new Size(109, 28);
+            btnAbout.TabIndex = 6;
+            btnAbout.Text = "关于超级工具箱";
+            toolTip1.SetToolTip(btnAbout, "关于此工具的说明");
+            btnAbout.UseVisualStyleBackColor = true;
+            // 
+            // SuperToolbox
+            // 
             AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(888, 571);
+            Controls.Add(btnAbout);
             Controls.Add(btnClear);
             Controls.Add(richTextBox1);
             Controls.Add(btnExtract);
@@ -142,5 +181,6 @@ namespace super_toolbox
         private System.Windows.Forms.ToolStripMenuItem deleteCategoryMenuItem;
         private System.Windows.Forms.ToolStripMenuItem moveToCategoryMenuItem;
         private ToolTip toolTip1;
+        private Button btnAbout;
     }
 }
