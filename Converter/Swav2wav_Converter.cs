875d4841c0d729741e86dcb2be34440e781d2856
Neptune <595554963@qq.com>
2026-03-04 15:10:13 +0800

    Update Swav2wav_Converter.cs
    成功实现对8-bit 带符号PCM、16-bit小端序PCM和IMA 4-bit ADPCM三种编码的swav解码

--- Converter/Swav2wav_Converter.cs
+++ Converter/Swav2wav_Converter.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using VGAudio.Containers.Wave;
 using VGAudio.Formats.Pcm16;
 
@@ -76,39 +77,10 @@ namespace super_toolbox
 
                         try
                         {
-                            byte[] swavData = File.ReadAllBytes(swavFilePath);
-
-                            if (swavData.Length < 0x18 ||
-                                BitConverter.ToUInt32(swavData, 0x00) != 0x56415753 ||
-                                BitConverter.ToUInt16(swavData, 0x04) != 0xFEFF ||
-                                BitConverter.ToUInt16(swavData, 0x0C) != 0x0010 ||
-                                BitConverter.ToUInt32(swavData, 0x10) != 0x41544144)
-                            {
-                                throw new InvalidDataException("无效的SWAV文件格式");
-                            }
-
-                            byte[] sampData = new byte[swavData.Length - 0x18];
-                            Array.Copy(swavData, 0x18, sampData, 0, sampData.Length);
-
-                            var swavInfo = ParseSampHeader(sampData);
-
-                            short[] pcmData = DecodeSamples(swavInfo);
-
-                            short[][] stereoChannels = new short[2][];
-                            stereoChannels[0] = pcmData;
-                            stereoChannels[1] = new short[pcmData.Length];
-                            Array.Copy(pcmData, stereoChannels[1], pcmData.Length);
-
-                            var stereoFormat = new Pcm16Format(stereoChannels, swavInfo.Rate);
-                            var wavWriter = new WaveWriter();
-
-                            using (var outFs = File.Create(outputWavPath))
-                            {
-                                wavWriter.WriteToStream(stereoFormat, outFs);
-                            }
+                            ConvertSingleSwavToWav(swavFilePath, outputWavPath);
 
                             successCount++;
-                            ConversionProgress?.Invoke(this, $"已转换:{fileName}.wav(立体声)");
+                            ConversionProgress?.Invoke(this, $"已转换:{fileName}.wav");
                             OnFileConverted(outputWavPath);
                         }
                         catch (Exception ex)
@@ -140,125 +112,149 @@ namespace super_toolbox
             ExtractAsync(directoryPath).Wait();
         }
 
-        private SwavInfo ParseSampHeader(byte[] data)
+        private void ConvertSingleSwavToWav(string swavFilePath, string outputWavPath)
         {
-            if (data.Length < 0x0C)
-                throw new InvalidDataException("无效的SAMP头");
+            using (var fs = new FileStream(swavFilePath, FileMode.Open, FileAccess.Read))
+            using (var br = new BinaryReader(fs))
+            {
+                string swavMagic = Encoding.ASCII.GetString(br.ReadBytes(4));
+                if (swavMagic != "SWAV")
+                    throw new InvalidDataException("无效的SWAV文件格式");
 
-            var info = new SwavInfo();
+                uint magic = br.ReadUInt32();
+                uint fileSize = br.ReadUInt32();
+                ushort headerSize = br.ReadUInt16();
+                ushort blockCount = br.ReadUInt16();
 
-            info.WaveType = data[0x00];
-            info.HasLoop = data[0x01] != 0;
-            info.Rate = BitConverter.ToUInt16(data, 0x02);
-            info.Time = BitConverter.ToUInt16(data, 0x04);
+                string dataMagic = Encoding.ASCII.GetString(br.ReadBytes(4));
+                if (dataMagic != "DATA")
+                    throw new InvalidDataException("无效的DATA块格式");
 
-            int loopStartInBytes = BitConverter.ToUInt16(data, 0x06) * 4;
-            int loopLenInBytes = BitConverter.ToInt32(data, 0x08) * 4;
+                uint dataChunkSize = br.ReadUInt32();
 
-            int firstSampOfs = info.WaveType == WAVE_ADPCM ? 4 : 0;
-            int byteToSampMul = info.WaveType == WAVE_ADPCM ? 2 : 1;
-            int byteToSampDiv = info.WaveType == WAVE_PCM16 ? 2 : 1;
+                byte waveType = br.ReadByte();
+                byte loopFlag = br.ReadByte();
+                ushort sampleRate = br.ReadUInt16();
+                ushort time = br.ReadUInt16();
+                ushort loopOffset = br.ReadUInt16();
+                uint nonLoopLen = br.ReadUInt32();
 
-            int loopStart = (loopStartInBytes - firstSampOfs) * byteToSampMul / byteToSampDiv;
-            int loopLen = loopLenInBytes * byteToSampMul / byteToSampDiv;
+                byte[] audioData = br.ReadBytes((int)(dataChunkSize - 0x14));
 
-            info.LoopStart = info.HasLoop ? loopStart : 0;
-            info.NumSamples = loopStart + loopLen;
-            info.DataOffset = 0x0C;
-            info.DataSize = loopStartInBytes + loopLenInBytes;
+                short[] pcmData = DecodeAudioData(audioData, waveType, nonLoopLen);
 
-            if (info.DataOffset + info.DataSize > data.Length)
-                throw new InvalidDataException("SAMP数据长度不足");
+                short[][] stereoChannels = new short[2][];
+                stereoChannels[0] = pcmData;
+                stereoChannels[1] = new short[pcmData.Length];
+                Array.Copy(pcmData, stereoChannels[1], pcmData.Length);
 
-            info.Data = new byte[info.DataSize];
-            Array.Copy(data, info.DataOffset, info.Data, 0, info.DataSize);
+                var stereoFormat = new Pcm16Format(stereoChannels, sampleRate);
+                var wavWriter = new WaveWriter();
 
-            return info;
+                using (var outFs = File.Create(outputWavPath))
+                {
+                    wavWriter.WriteToStream(stereoFormat, outFs);
+                }
+            }
         }
 
-        private short[] DecodeSamples(SwavInfo info)
+        private short[] DecodeAudioData(byte[] data, int waveType, uint sampleCount)
         {
-            int bps = info.WaveType == WAVE_PCM8 ? 8 : 16;
-            short[] result = new short[info.NumSamples];
-
-            if (info.WaveType == WAVE_PCM8)
+            switch (waveType)
             {
-                for (int i = 0; i < info.NumSamples && i < info.DataSize; i++)
-                {
-                    result[i] = (short)((info.Data[i] ^ 0x80) << 8);
-                }
+                case WAVE_PCM8:
+                    return DecodePcm8(data);
+                case WAVE_PCM16:
+                    return DecodePcm16(data);
+                case WAVE_ADPCM:
+                    return DecodeImaAdpcm(data, sampleCount);
+                default:
+                    throw new NotSupportedException($"不支持的音频格式:{waveType}");
             }
-            else if (info.WaveType == WAVE_PCM16)
+        }
+
+        private short[] DecodePcm8(byte[] data)
+        {
+            short[] result = new short[data.Length];
+            for (int i = 0; i < data.Length; i++)
             {
-                for (int i = 0; i < info.NumSamples && i * 2 + 1 < info.DataSize; i++)
-                {
-                    result[i] = BitConverter.ToInt16(info.Data, i * 2);
-                }
+                result[i] = (short)((sbyte)data[i] << 8);
             }
-            else if (info.WaveType == WAVE_ADPCM)
-            {
-                int sample = BitConverter.ToInt16(info.Data, 0);
-                int stepIndex = info.Data[2];
-                bool low = true;
-                int dataPos = 4;
-
-                for (int i = 0; i < info.NumSamples; i++)
-                {
-                    result[i] = (short)sample;
-
-                    if (dataPos >= info.DataSize)
-                        break;
-
-                    byte code = info.Data[dataPos];
-
-                    if (!low)
-                    {
-                        code >>= 4;
-                        dataPos++;
-                    }
-
-                    code &= 0x0F;
-
-                    int step = ADPCMTable[stepIndex];
-                    int diff = step >> 3;
-
-                    if ((code & 1) != 0) diff += step >> 2;
-                    if ((code & 2) != 0) diff += step >> 1;
-                    if ((code & 4) != 0) diff += step;
-
-                    if ((code & 8) != 0)
-                    {
-                        sample -= diff;
-                        if (sample < -32767) sample = -32767;
-                    }
-                    else
-                    {
-                        sample += diff;
-                        if (sample > 32767) sample = 32767;
-                    }
-
-                    stepIndex += IMAIndexTable[code];
-                    if (stepIndex < 0) stepIndex = 0;
-                    if (stepIndex > 88) stepIndex = 88;
-
-                    low = !low;
-                }
-            }
-
             return result;
         }
 
-        private class SwavInfo
+        private short[] DecodePcm16(byte[] data)
         {
-            public int WaveType { get; set; }
-            public bool HasLoop { get; set; }
-            public int Rate { get; set; }
-            public int Time { get; set; }
-            public int LoopStart { get; set; }
-            public int NumSamples { get; set; }
-            public int DataOffset { get; set; }
-            public int DataSize { get; set; }
-            public byte[] Data { get; set; } = Array.Empty<byte>();
+            if (data.Length % 2 != 0)
+                throw new InvalidDataException("PCM16数据长度必须是2的倍数");
+
+            short[] result = new short[data.Length / 2];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = BitConverter.ToInt16(data, i * 2);
+            }
+            return result;
+        }
+
+        private short[] DecodeImaAdpcm(byte[] data, uint expectedSampleCount)
+        {
+            if (data.Length < 4)
+                throw new InvalidDataException("ADPCM数据长度不足");
+
+            int predictor = BitConverter.ToInt16(data, 0);
+            int stepIndex = data[2];
+
+            stepIndex = Math.Clamp(stepIndex, 0, 88);
+
+            var pcmSamples = new System.Collections.Generic.List<short>();
+
+            int byteIndex = 4;
+            bool useHighNibble = false;
+
+            for (int i = 0; i < expectedSampleCount; i++)
+            {
+                pcmSamples.Add((short)predictor);
+
+                if (byteIndex >= data.Length) break;
+
+                byte currentByte = data[byteIndex];
+                int nibble;
+
+                if (!useHighNibble)
+                {
+                    nibble = currentByte & 0x0F;
+                    useHighNibble = true;
+                }
+                else
+                {
+                    nibble = (currentByte >> 4) & 0x0F;
+                    useHighNibble = false;
+                    byteIndex++;
+                }
+
+                int step = ADPCMTable[stepIndex];
+                int delta = step >> 3;
+
+                if ((nibble & 1) != 0) delta += step >> 2;
+                if ((nibble & 2) != 0) delta += step >> 1;
+                if ((nibble & 4) != 0) delta += step;
+
+                if ((nibble & 8) != 0)
+                {
+                    predictor -= delta;
+                }
+                else
+                {
+                    predictor += delta;
+                }
+
+                predictor = Math.Clamp(predictor, short.MinValue, short.MaxValue);
+
+                stepIndex += IMAIndexTable[nibble];
+                stepIndex = Math.Clamp(stepIndex, 0, 88);
+            }
+
+            return pcmSamples.ToArray();
         }
     }
 }
